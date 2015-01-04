7ed61c0f91f567c4924aa4fbe32d7bb93d195c86
elc1798 <subliminalmau5@gmail.com>
2015-01-03 21:41:47 -0500

    Fixed typo

--- SpamCarver-70/Solution.sh
+++ SpamCarver-70/Solution.sh
@@ -1,4 +1,4 @@
 echo "Bash one-liner: "
-sudo apt-get install foremost ; foremost spamcarger.jpg ; cd output/zip ; unzip *.zip ; mv \ \ \ \ \ \ \ \  UNHIDE.jpg
+sudo apt-get install foremost ; foremost spamcarver.jpg ; cd output/zip ; unzip *.zip ; mv \ \ \ \ \ \ \ \  UNHIDE.jpg
 echo "View UNHIDE.jpg :) "
 echo "Key: 7adf6f07e0810003c585a7be97868a90"
